83f287b3294e38ea4c6ff18c953b778bbdd32daa
Kristopher Betz <kjbetz@new-host-2.home>
2020-08-10 23:56:30 -0400

    Component test (finally) passing!

--- libs/books/feature/src/lib/book-search/book-search-results-item/book-search-results-item.component.spec.ts
+++ libs/books/feature/src/lib/book-search/book-search-results-item/book-search-results-item.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { BookSearchResultsItemComponent } from './book-search-results-item.component';
 import { SharedTestingModule } from '@tmo/shared/testing';
 import { BooksFeatureModule } from '../../books-feature.module';
+import { ReadingListBook } from '@tmo/books/data-access';
 
 describe('BookSearchResultsItemComponent', () => {
   let component: BookSearchResultsItemComponent;
@@ -17,6 +18,16 @@ describe('BookSearchResultsItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BookSearchResultsItemComponent);
     component = fixture.componentInstance;
+
+    const book: ReadingListBook = {
+      id: '1',
+      title: 'My Book',
+      authors: ['Author, First', 'Author, Second'],
+      description: 'This is my test book',
+      isAdded: false
+     };
+
+    component.book = book;
     fixture.detectChanges();
   });
 
--- libs/books/feature/src/lib/reading-list/reading-list-item/reading-list-item.component.spec.ts
+++ libs/books/feature/src/lib/reading-list/reading-list-item/reading-list-item.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReadingListItemComponent } from './reading-list-item.component';
 import { SharedTestingModule } from '@tmo/shared/testing';
 import { BooksFeatureModule } from '../../books-feature.module';
+import { ReadingListItem } from '@tmo/shared/models';
 
 describe('ReadingListItemComponent', () => {
   let component: ReadingListItemComponent;
@@ -17,6 +18,16 @@ describe('ReadingListItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ReadingListItemComponent);
     component = fixture.componentInstance;
+
+    const book: ReadingListItem = {
+      bookId: '1',
+      title: 'My Book',
+      authors: ['Author, First', 'Author, Second'],
+      description: 'This is my test book',
+    }
+
+    component.book = book;
+
     fixture.detectChanges();
   });
 
